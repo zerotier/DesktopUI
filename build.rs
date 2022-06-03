72132ad6bf44186e0017dcb661cd4feca14f8509
Grant Limberg <grant.limberg@zerotier.com>
2022-06-03 14:04:22 -0700

    build on Linux

--- build.rs
+++ build.rs
@@ -9,4 +9,11 @@ fn main() {
     );
     println!("cargo:rustc-link-lib=static=zt_desktop_tray");
     println!("cargo:rustc-link-lib=static=ui");
+    #[cfg(target_os = "linux")] {
+        println!("cargo:rustc-link-lib=dylib=gtk-3");
+        println!("cargo:rustc-link-lib=dylib=gdk-3");
+        println!("cargo:rustc-link-lib=dylib=gobject-2.0");
+        println!("cargo:rustc-link-lib=dylib=glib-2.0");
+        println!("cargo:rustc-link-lib=dylib=appindicator3");
+    }
 }
